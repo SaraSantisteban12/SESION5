75d8296c10da675a1399860dfad4d42d7c65b1f2
SaraSantisteban12 <166246482+SaraSantisteban12@users.noreply.github.com>
2024-04-27 14:04:31 -0500

    Add files via upload

---
+++ src/sesion5/Sesion5.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sesion5;
+
+import sesion5.entidades.pantallas.PantallaPersonas;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Sesion5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Crea el objeto de JFrame
+        PantallaPersonas pantalla = new PantallaPersonas();
+        //Muestra el JRrame
+        pantalla.setVisible(true);
+        //Centra el JFrame en la pantalla
+        pantalla.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ src/sesion5/entidades/Cliente.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sesion5.entidades;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Cliente  extends Persona{
+    
+    private String codCliente;
+
+    public static Cliente fromPersona(Persona per){
+        Cliente cli = new Cliente();
+        cli.setNombre(per.getNombre());
+        cli.setApellido(per.getApellido());
+        cli.setDni(per.getDni());
+        return cli;     
+    }
+    public Cliente() {}
+     
+     
+    public Cliente(String codCliente) {
+        this.codCliente = codCliente;
+    }
+    
+    @Override
+     public String getText(){
+       return "CLIENTE: "+
+               this.getNombre() + " " +
+               this.getApellido() + " |DNI-CLI: "+
+               this.getDni();
+     }
+    
+    public String getCodCliente() {
+        return codCliente;
+    }
+
+        public void setCodCliente(String codCliente) {
+        this.codCliente = codCliente;
+    }
+    
+}
